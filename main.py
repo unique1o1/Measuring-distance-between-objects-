5b344aab2d0783387616a93a4d646db067b265d9
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-10-11 23:56:27 +0545

    cmd line argument and convert image to grayscale, blur

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ ap.add_argument("-i", "--image", required=True,
                 help="path to the input image")
 ap.add_argument("-w", "--width", type=float, required=True,
                 help="width of the left-most object in the image (in inches)")
-args = vars(ap.parse_args())
+args = ap.parse_args()
+image = cv2.imread(args.image)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (7, 7), 0)
