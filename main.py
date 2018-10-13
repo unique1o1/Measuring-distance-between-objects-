1b6a5061f9f0eb3bf3dc9414b1aaada2664471a4
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-10-13 12:48:19 +0545

    sort contours from  left most object

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import numpy as np
 import argparse
 import cv2
 
+
+def sort_contours(cnts, *, reverse=False):
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda x: x[1][0], reverse=False))
+    return cnts
+
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
                 help="path to the input image")
@@ -12,3 +21,17 @@ args = ap.parse_args()
 image = cv2.imread(args.image)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+# perform edge detection, then perform a dilation + erosion to
+# close gaps in between object edges
+edged = cv2.Canny(gray, 50, 100)
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)
+
+# find contours in the edge map
+cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)[1]
+cnts = sort_contours(cnts, reverse=False)
+colors = ((0, 0, 255), (240, 0, 159), (0, 165, 255), (255, 255, 0),
+          (255, 0, 255))
+refobj = None
